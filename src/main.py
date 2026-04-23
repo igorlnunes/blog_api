af7414aa29dcaef7679b29e448f9333569c17f2c
Igor de Lima Nunes <igorlnunes@gmail.com>
2026-04-23 10:11:49 -0300

    fix: CORS issues

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.controllers import auth, post
 from src.database import database, engine, metadata
@@ -66,6 +67,7 @@ Você será capaz de fazer:
 )
 
 app.add_middleware(
+    CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
